beabf730a0f4be50771d98a3b7443484b06df8fa
APJifengc <1500533774@qq.com>
2021-01-26 09:25:53 +0800

    :tada: Init: Init the project

---
+++ src/main/java/io/github/apjifengc/yaresourcepackmanager/YaResourcepackManager.java
@@ -0,0 +1,17 @@
+package io.github.apjifengc.yaresourcepackmanager;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class YaResourcepackManager extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
